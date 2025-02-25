03f1fb1be919230ce4a889acc4fe240f8354da65
David Cox <cox407@purdue.edu>
2025-02-24 22:02:34 -0500

    got email conf to work and changed default address

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -7,6 +7,8 @@ from flask_mailman import EmailMessage
 import uuid
 #all REST API calls for login/auth
 
+import traceback
+
 
 auth_bp = Blueprint('auth', __name__, template_folder="../templates")
 
@@ -19,7 +21,7 @@ def send_email(to_email, subject, message):
     email.send()
     return "Email sent!"
 
-def send_email_route(uuidUser, username, to_email="7808steven@gmail.com"):
+def send_email_route(uuidUser, username, to_email="cox407@purdue.edu"):
     # Get email data from the request JSON
     if not to_email:
         return jsonify({"error": "Email is required"}), 400  # Return error if email is missing
@@ -34,7 +36,7 @@ def send_email_route(uuidUser, username, to_email="7808steven@gmail.com"):
     except Exception as e:
         return jsonify({"error": str(e)}), 500  # Error handling
 
-def send_email_password_reset(uuidUser, username, to_email="7808steven@gmail.com"):
+def send_email_password_reset(uuidUser, username, to_email="cox407@purdue.edu"):
     if not to_email:
         return jsonify({"error": "Email is required"}), 400  # Return error if email is missing
 
@@ -67,7 +69,9 @@ def register():
         db.session.commit()
         send_email_route(user.uuid_user, user.username)
         return jsonify({'message': 'User registered!', 'id': user.id})
-    except:
+    except Exception as e:
+        print(f"Error: {e}")
+        traceback.print_exc()
         return jsonify({'message': 'duplicate'})
 
 @auth_bp.route('/login', methods=['POST'])
@@ -150,7 +154,7 @@ def password_reset_post_confirm(user_uuid):
     hashed_password = bcrypt.generate_password_hash(new_password).decode('utf-8')
     user.password = hashed_password
     db.session.commit()
-    send_email("7808steven@gmail.com", "Password Reset", "Hello! You are receiving this email because you recently reset your password.") #temporary value of my email
+    send_email("cox407@purdue.edu", "Password Reset", "Hello! You are receiving this email because you recently reset your password.") #temporary value of my email
     return render_template("confirmation.html", message="Successfully reset password!")
 
 @auth_bp.route('/password_reset/<uuid:user_uuid>', methods=['GET'])
--- create_env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export USER="root"
-export DB_PASSWORD='8PDjjplfuVvJWRGK'
-export HOST="127.0.0.1"
-export DATABASE="users"
